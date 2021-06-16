ff1cb58733ddfdf0abef50beb487a90d9d7beff7
Scott White <spwhite1337@gmail.com>
2021-06-15 22:09:54 -0400

    Upgrade for new format

--- tweets.py
+++ tweets.py
@@ -6,47 +6,32 @@ import pandas as pd
 
 def get_all_tweets(screen_name, keys):
     # Twitter only allows access to a users most recent 3240 tweets with this method
-
     # authorize twitter, initialize tweepy
     auth = tweepy.OAuthHandler(keys['consumer_key'], keys['consumer_secret'])
     auth.set_access_token(keys['access_token_key'], keys['access_token_secret'])
     api = tweepy.API(auth)
-
     # initialize a list to hold all the tweepy Tweets
     alltweets = []
-
     # make initial request for most recent tweets (200 is the maximum allowed count)
     new_tweets = api.user_timeline(screen_name=screen_name, count=200, tweet_mode='extended')
-
     # save most recent tweets
     alltweets.extend(new_tweets)
-
     # save the id of the oldest tweet less one
     oldest = alltweets[-1].id - 1
-
     # keep grabbing tweets until there are no tweets left to grab
     while len(new_tweets) > 0:
         print(f"getting tweets before {oldest}")
-
         # all subsequent requests use the max_id param to prevent duplicates
         new_tweets = api.user_timeline(screen_name=screen_name, count=200, max_id=oldest, tweet_mode='extended')
-
         # save most recent tweets
         alltweets.extend(new_tweets)
-
         # update the id of the oldest tweet less one
         oldest = alltweets[-1].id - 1
-
         print(f"...{len(alltweets)} tweets downloaded so far")
 
     # Convert to list of Jsons
     tweet_jsons = [tweet._json for tweet in alltweets]
-
-    # Save jsons
-    # with open(f'{screen_name}_tweets.pkl', 'wb') as fp:
-    #     pickle.dump(tweet_jsons, fp)
-
-    # Get team as first user mention
+    # Get team as first user mention, team-name as name of twitter account for first user-mention
     user_mentions, team_names = [], []
     for tweet in tweet_jsons:
         mentions = tweet.get('entities', []).get('user_mentions', [])
@@ -57,36 +42,70 @@ def get_all_tweets(screen_name, keys):
             user_mentions.append('None')
             team_names.append('None')
 
-    # Get departures, arrivals, flight nos, tail nos, aircrafts by parsing tweet
-    departures, arrivals = [], []
-    flight_nos, tail_nos, aircrafts = [], [], []
-    for tweet in tweet_jsons:
-        split = tweet['full_text'].split('\n')
-        if len(split) > 1:
-            departures.append(split[1][:3])
-            arrivals.append(split[1][-3:])
-        else:
-            departures.append('None')
-            arrivals.append('None')
+    def get_tail_number(tweet: str) -> str:
+        flight_info = tweet.split('\n')
+        if len(flight_info) >= 5:
+            flight_info = flight_info[-4][1:]  # Drop emoji in front
+            flight_info = flight_info.split('|')
+            if len(flight_info) >= 3:
+                return flight_info[0].strip()[1:]
+        return 'None'
 
-        if len(split) > 2:
-            pipe_split = split[2].split('|')
-            if len(pipe_split) > 1:
-                flight_nos.append(pipe_split[0])
-                tail_nos.append(pipe_split[1])
-            else:
-                flight_nos.append('None')
-                tail_nos.append('None')
-            if len(pipe_split) > 2:
-                aircrafts.append(pipe_split[2])
-            else:
-                aircrafts.append('None')
-        else:
-            flight_nos.append('None')
-            tail_nos.append('None')
-            aircrafts.append('None')
+    def get_flight_no(tweet: str) -> str:
+        flight_info = tweet.split('\n')
+        if len(flight_info) >= 5:
+            flight_info = flight_info[-4][1:]  # Drop emoji in front
+            flight_info = flight_info.split('|')
+            if len(flight_info) >= 3:
+                return flight_info[1].strip()
+        return 'None'
 
-    # Get first link
+    def get_aircraft_type(tweet: str) -> str:
+        flight_info = tweet.split('\n')
+        if len(flight_info) >= 5:
+            flight_info = flight_info[-4][1:]  # Drop emoji in front
+            flight_info = flight_info.split('|')
+            if len(flight_info) >= 3:
+                return flight_info[2].strip()
+        return 'None'
+
+    def get_departure(tweet: str) -> str:
+        flight_info = tweet.split('\n')
+        if len(flight_info) >= 3:
+            flight_info = flight_info[-3][1:]  # Drop emoji in front
+            flight_info = flight_info.split(' - ')
+            if len(flight_info) >= 1:
+                return flight_info[0].strip()
+        return 'None'
+
+    def get_departure_time(tweet: str) -> str:
+        flight_info = tweet.split('\n')
+        if len(flight_info) >= 3:
+            flight_info = flight_info[-3][1:]  # Drop emoji in front
+            flight_info = flight_info.split(' - ')
+            if len(flight_info) >= 2:
+                return flight_info[1].strip()
+        return 'None'
+
+    def get_arrival(tweet: str) -> str:
+        flight_info = tweet.split('\n')
+        if len(flight_info) >= 3:
+            flight_info = flight_info[-2][1:]  # Drop emoji in front
+            flight_info = flight_info.split(' - ')
+            if len(flight_info) >= 1:
+                return flight_info[0].strip()
+        return 'None'
+
+    def get_arrival_time(tweet: str) -> str:
+        flight_info = tweet.split('\n')
+        if len(flight_info) >= 3:
+            flight_info = flight_info[-2][1:]  # Drop emoji in front
+            flight_info = flight_info.split(' - ')
+            if len(flight_info) >= 2:
+                return flight_info[1].strip()
+        return 'None'
+
+    # Get first link for flightware
     flightware_links = []
     for tweet in tweet_jsons:
         urls = tweet.get('entities', []).get('urls', [])
@@ -96,23 +115,29 @@ def get_all_tweets(screen_name, keys):
             flightware_links.append('None')
 
     # Save as xlsx
-    df_tweets = pd.DataFrame()
-    df_tweets['created_at'] = [tweet['created_at'] for tweet in tweet_jsons]
-    df_tweets['tweet_date'] = pd.to_datetime(df_tweets['created_at']).dt.date
-    df_tweets['text'] = [tweet['full_text'] for tweet in tweet_jsons]
-    df_tweets['first_user_mention'] = user_mentions
-    df_tweets['team_name'] = team_names
-    df_tweets['departures'] = departures
-    df_tweets['arrivals'] = arrivals
-    df_tweets['flight_nos'] = flight_nos
-    df_tweets['tail_nos'] = tail_nos
-    df_tweets['aircrafts'] = aircrafts
-    df_tweets['flightware_links'] = flightware_links
-    df_tweets['retweets'] = [tweet['retweet_count'] for tweet in tweet_jsons]
-    df_tweets['favorite_count'] = [tweet['favorite_count'] for tweet in tweet_jsons]
+    df = pd.DataFrame()
+    df['created_at'] = [tweet['created_at'] for tweet in tweet_jsons]
+    df['tweet_date'] = pd.to_datetime(df['created_at']).dt.date
+    df['text'] = [tweet['full_text'] for tweet in tweet_jsons]
+    df['first_user_mention'] = user_mentions
+    df['team_name'] = team_names
+
+    df['aircraft_type'] = df['text'].apply(get_aircraft_type)
+    df['tail_number'] = df['text'].apply(get_tail_number)
+    df['flight_no'] = df['text'].apply(get_flight_no)
+    df['departure'] = df['text'].apply(get_departure)
+    df['arrival'] = df['text'].apply(get_arrival)
+    df['departure_time'] = df['text'].apply(get_departure_time)
+    df['arrival_time'] = df['text'].apply(get_arrival_time)
+
+    df['flightware_links'] = flightware_links
+    df['retweets'] = [tweet['retweet_count'] for tweet in tweet_jsons]
+    df['favorite_count'] = [tweet['favorite_count'] for tweet in tweet_jsons]
 
     # Save to excel
-    df_tweets.to_excel(f'{screen_name}_tweets.xlsx', index=False)
+    df = df[df['aircraft_type'] != 'None']
+    df.to_csv(f'{screen_name}_tweets.csv', index=False)
+    df.to_excel(f'{screen_name}_tweets.xlsx', index=False)
 
 
 if __name__ == '__main__':
