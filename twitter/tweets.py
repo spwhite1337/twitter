99f937303f319dc8b8a4ce03beafc693583c59d0
Scott White <spwhite1337@gmail.com>
2021-06-29 21:03:03 -0400

    to_date

--- twitter/tweets.py
+++ twitter/tweets.py
@@ -6,10 +6,11 @@ from twitter.api import TwitterPull
 def twitter_pull():
     parser = argparse.ArgumentParser()
     parser.add_argument('--screen_name', type=str, required=True)
+    parser.add_argument('--to_date', type=str, required=False, default=None)
     parser.add_argument('--overwrite', action='store_true')
     args = parser.parse_args()
 
     api = TwitterPull(screen_name=args.screen_name, overwrite=args.overwrite)
-    tweets = api.etl()
+    tweets = api.etl(to_date=args.to_date)
     df = api.parse_raw_tweets(tweets)
     api.save_parsed(df)
